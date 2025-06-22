4c8c3198141a9908db25ce812bfa3136bf97b08b
farza <farzanrichu610@gmail.com>
2025-06-22 19:54:41 +0530

    made the transfer fucntion for both PID and motor

--- transfer_fn.m
+++ transfer_fn.m
@@ -1,2 +1,30 @@
-%here i will document every values and transfer fucntions 
-%this script will be the testing area for the tfs
+%% Motor Transfer Function
+% Transfer function of the motor: 
+% G(s) = Kt / ( (Js + B)(Ls + R) + Kt*Kb )
+
+
+% Electrical parameters
+L = 0.5;     % Inductance (Henrys)
+R = 1.0;     % Resistance (Ohms)
+
+% Mechanical parameters
+J = 0.01;    % Moment of inertia of the rotor (kg*m^2)
+B = 0.1;     % Damping coefficient / Viscous friction (N*m*s)
+
+% Electromechanical constants
+Kt = 0.01;   % Motor Torque constant (N*m/A)
+Kb = 0.01;   % Back EMF constant (V*s/rad)
+
+
+% Denominator: (Js + B)(Ls + R) + Kt*Kb
+% We use conv() for polynomial multiplication
+mech=[J, B];
+elec=[L R];
+
+denom_tf=(conv(mech,elec)+Kt*Kb);
+num_tf=Kt;
+
+%tf of motor
+motor_tf=tf(num_tf,denom_tf);
+motor_tf
+
